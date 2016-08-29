2bc01bba0f290681e8b96d3f6bacc4f8d7068d85
Vinicius Moribe Pereira <vmp@viniciusmoribe.com>
2016-08-28 22:26:04 -0300

    Micronutrientes total

--- app/models/analise.rb
+++ app/models/analise.rb
@@ -5,7 +5,7 @@ class Analise < ActiveRecord::Base
 
 validates_presence_of :fazenda_id, :talhao_id, :situacao, :profundidade, :ano, :ph, :potassio_k, :fosforo_p, :sodio_na, :calcio_ca, :magnesio_mg, :aluminio_al, :h_al, :mat_organica, :p_rem, :zinco_zn, :ferro_fe, :manganes_mn, :cobre_cu, :boro_b, :enxofre_s
 
-#fertilizantes
+#Relações
 
 
 

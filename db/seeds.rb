6b3b74a4731a31ab1ed76b9194d8b7a11a032e2a
Vinicius Moribe Pereira <vmp@viniciusmoribe.com>
2017-08-12 21:40:40 -0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,6 +49,17 @@ Talhao.create!([
   {fazenda_id: 3, nome: "L4 - A", cultura: "Café", variedade: "Catuai", ano_plantio: 2014, area: "3.71", esp_ruas: "3.6", esp_plantas: "0.75", argila: 0, silte: 0, areia: 0, user_id: 1},
   {fazenda_id: 3, nome: "L5 - A", cultura: "Café", variedade: "Catuai", ano_plantio: 2016, area: "11.5", esp_ruas: "3.6", esp_plantas: "0.75", argila: 0, silte: 0, areia: 0, user_id: 1}
 ])
+Vargesso.create!([
+  {user_id: 1, ano: 2016, profundidade: "35.0", areaAplicacao: "75.0", teorCalcio: "20.0"},
+  {user_id: 1, ano: 2017, profundidade: "35.0", areaAplicacao: "75.0", teorCalcio: "20.0"}
+])
+Variavel.create!([
+  {user_id: 1, ano: 2016, profundidade: "7.0", areaAplicacao: "100.0", prnt: "90.0", produto_id: 29},
+  {user_id: 1, ano: 2014, profundidade: "7.0", areaAplicacao: "75.0", prnt: "90.0", produto_id: 28},
+  {user_id: 1, ano: 2013, profundidade: "7.0", areaAplicacao: "75.0", prnt: "90.0", produto_id: 28},
+  {user_id: 1, ano: 2015, profundidade: "10.0", areaAplicacao: "100.0", prnt: "90.0", produto_id: 28},
+  {user_id: 1, ano: 2017, profundidade: "10.0", areaAplicacao: "100.0", prnt: "87.03", produto_id: 28}
+])
 Parametro.create!([
   {ano: 2013, parcelamento: 3, producao_esperada: "21 - 30", user_id: 1},
   {ano: 2014, parcelamento: 4, producao_esperada: "31 - 40", user_id: 1},
@@ -68,6 +79,9 @@ Produto.create!([
   {user_id: 1, nome: "Biorin Plus", tipo: "Nutrição", preco: "940.0", custo_op: "80.0", nitrogenio_n: "0.0", fosforo_p2o5: "18.0", potassio_k2o: "0.0", calcio_ca: "15.0", magnesio_mg: "0.0", enxofre_s: "0.0", boro_b: "0.1", cobre_cu: "0.05", ferro_fe: "0.0", manganes_mn: "0.1", zinco_zn: "0.5", ano: 2016, cao: nil, mgo: nil, prnt: nil},
   {user_id: 1, nome: "Ferthium Phós HF", tipo: "Nutrição", preco: "1200.0", custo_op: "80.0", nitrogenio_n: "3.0", fosforo_p2o5: "16.0", potassio_k2o: "6.0", calcio_ca: "0.0", magnesio_mg: "1.0", enxofre_s: "3.0", boro_b: "0.1", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.05", zinco_zn: "0.15", ano: 2016, cao: nil, mgo: nil, prnt: nil},
   {user_id: 1, nome: "Ferthium NK + S", tipo: "Nutrição", preco: "1200.0", custo_op: "80.0", nitrogenio_n: "8.0", fosforo_p2o5: "0.0", potassio_k2o: "10.0", calcio_ca: "0.0", magnesio_mg: "0.0", enxofre_s: "8.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2016, cao: nil, mgo: nil, prnt: nil},
+  {user_id: 1, nome: "Calcário Dolomítico C", tipo: "Corretivo", preco: "52.0", custo_op: "60.0", nitrogenio_n: "0.0", fosforo_p2o5: "0.0", potassio_k2o: "0.0", calcio_ca: "24.03", magnesio_mg: "10.64", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2016, cao: "33.65", mgo: "17.74", prnt: "85.72"},
+  {user_id: 1, nome: "Calcário Dolomítico B", tipo: "Corretivo", preco: "45.0", custo_op: "60.0", nitrogenio_n: "0.0", fosforo_p2o5: "0.0", potassio_k2o: "0.0", calcio_ca: "28.36", magnesio_mg: "8.03", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2016, cao: "39.7", mgo: "13.38", prnt: "87.03"},
+  {user_id: 1, nome: "Calcário Dolomítico A", tipo: "Corretivo", preco: "38.0", custo_op: "60.0", nitrogenio_n: "0.0", fosforo_p2o5: "0.0", potassio_k2o: "0.0", calcio_ca: "32.05", magnesio_mg: "5.61", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2016, cao: "44.86", mgo: "9.35", prnt: "91.31"},
   {user_id: 1, nome: "10--10--10", tipo: "Nutrição", preco: "1800.0", custo_op: "80.0", nitrogenio_n: "10.0", fosforo_p2o5: "10.0", potassio_k2o: "10.0", calcio_ca: "0.0", magnesio_mg: "0.0", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2017, cao: nil, mgo: nil, prnt: nil},
   {user_id: 1, nome: "Superfosfato Simples", tipo: "Nutrição", preco: "800.0", custo_op: "80.0", nitrogenio_n: "0.0", fosforo_p2o5: "19.0", potassio_k2o: "0.0", calcio_ca: "0.0", magnesio_mg: "0.0", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2017, cao: nil, mgo: nil, prnt: nil},
   {user_id: 1, nome: "Mono Amônio Fosfato (MAP)", tipo: "Nutrição", preco: "1800.0", custo_op: "80.0", nitrogenio_n: "11.0", fosforo_p2o5: "52.0", potassio_k2o: "0.0", calcio_ca: "0.0", magnesio_mg: "0.0", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2017, cao: nil, mgo: nil, prnt: nil},
@@ -86,21 +100,6 @@ Produto.create!([
   {user_id: 1, nome: "Calcário Dolomítico B", tipo: "Corretivo", preco: "45.0", custo_op: "60.0", nitrogenio_n: "0.0", fosforo_p2o5: "0.0", potassio_k2o: "0.0", calcio_ca: "28.36", magnesio_mg: "8.03", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2017, cao: "39.7", mgo: "13.38", prnt: "87.03"},
   {user_id: 1, nome: "Calcário Dolomítico A", tipo: "Corretivo", preco: "38.0", custo_op: "60.0", nitrogenio_n: "0.0", fosforo_p2o5: "0.0", potassio_k2o: "0.0", calcio_ca: "32.05", magnesio_mg: "5.61", enxofre_s: "0.0", boro_b: "0.0", cobre_cu: "0.0", ferro_fe: "0.0", manganes_mn: "0.0", zinco_zn: "0.0", ano: 2017, cao: "44.86", mgo: "9.35", prnt: "91.31"}
 ])
-
-Vargesso.create!([
-  {user_id: 1, ano: 2016, profundidade: "35.0", areaAplicacao: "75.0", teorCalcio: "20.0"},
-  {user_id: 1, ano: 2017, profundidade: "35.0", areaAplicacao: "75.0", teorCalcio: "20.0"}
-])
-Variavel.create!([
-  {user_id: 1, ano: 2016, profundidade: "7.0", areaAplicacao: "100.0", prnt: "90.0", produto_id: 29},
-  {user_id: 1, ano: 2014, profundidade: "7.0", areaAplicacao: "75.0", prnt: "90.0", produto_id: 28},
-  {user_id: 1, ano: 2013, profundidade: "7.0", areaAplicacao: "75.0", prnt: "90.0", produto_id: 28},
-  {user_id: 1, ano: 2015, profundidade: "10.0", areaAplicacao: "100.0", prnt: "90.0", produto_id: 28},
-  {user_id: 1, ano: 2017, profundidade: "10.0", areaAplicacao: "100.0", prnt: "87.03", produto_id: 28}
-])
-
-
-
 Analise.create!([
   {user_id: 1, fazenda_id: 1, talhao_id: 2, situacao: "Produção", profundidade: "0-20", ano: 2013, ph: "5.3", potassio_k: "76.0", fosforo_p: "7.21", sodio_na: "0.0", calcio_ca: "1.1", magnesio_mg: "0.4", aluminio_al: "0.1", h_al: "3.24", mat_organica: "2.23", p_rem: "20.22", zinco_zn: "0.99", ferro_fe: "34.07", manganes_mn: "4.1", cobre_cu: "1.56", boro_b: "0.28", enxofre_s: "49.14"},
   {user_id: 1, fazenda_id: 1, talhao_id: 3, situacao: "Produção", profundidade: "0-20", ano: 2013, ph: "5.6", potassio_k: "106.0", fosforo_p: "19.68", sodio_na: "0.0", calcio_ca: "1.3", magnesio_mg: "0.5", aluminio_al: "0.1", h_al: "2.9", mat_organica: "2.87", p_rem: "25.07", zinco_zn: "1.87", ferro_fe: "44.57", manganes_mn: "5.06", cobre_cu: "1.63", boro_b: "0.31", enxofre_s: "33.06"},

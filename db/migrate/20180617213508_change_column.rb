ccfa266eac63c0c029ca5469016dc66e94a29ba9
Vinicius Moribe Pereira <vmp@viniciusmoribe.com>
2018-06-18 10:39:55 +0000

    migration

--- db/migrate/20180617213508_change_column.rb
+++ db/migrate/20180617213508_change_column.rb
@@ -1,5 +1,5 @@
 class ChangeColumndata_entrada < ActiveRecord::Migration
   def change
-    rename_column :lotes, :data_entrada, :data_entrada
+    rename_column :lotes, :dataEntrada, :data_entrada
   end
 end

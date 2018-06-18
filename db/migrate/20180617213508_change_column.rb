2ceb89585808dd8f03c97a0a2a91049ac80ecef1
Vinicius Moribe Pereira <vmp@viniciusmoribe.com>
2018-06-18 10:47:43 +0000

    def up e down teste

--- db/migrate/20180617213508_change_column.rb
+++ db/migrate/20180617213508_change_column.rb
@@ -1,5 +1,13 @@
 class ChangeColumndata_entrada < ActiveRecord::Migration
-  def change
+  def up
     rename_column :lotes, :dataEntrada, :data_entrada
   end
+
+  def down
+    rename_column :lotes, :data_entrada, :dataEntrada
+  end
+  
+  # def change
+  #   rename_column :lotes, :dataEntrada, :data_entrada
+  # end
 end

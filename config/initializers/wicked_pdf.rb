ef65b094be729360f974ba1b8f82f3a12569258d
Vinicius Moribe Pereira <vmp@viniciusmoribe.com>
2018-09-22 14:51:27 +0000

    PDF

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,1 +1,13 @@
-WickedPdf.config ||= {}
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-linux-amd64"
+else
+  # Linux (check your processor for Intel x86 or AMD x64)
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-i386"
+  # OS X
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-0.9.9-OS-X.i386"            
+  # Windows
+  # wkhtmltopdf_path = 'C:\Program Files/wkhtmltopdf/wkhtmltopdf.exe'
+end
+
+WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
